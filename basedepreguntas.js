422938f7ba6e3be95ae782198972f7c65d10948c
davidaliri <davidaliri95@gmail.com>
2024-10-16 14:39:12 -0500

    Add files via upload

---
+++ basedepreguntas.js
@@ -0,0 +1,159 @@
+basedepreguntas = [
+    {
+
+      pregunta: `Según el texto leído ¿Qué descubrimiento marca el paso de la Prehistoria a la Historia?`,
+      imagen: `https://i.ibb.co/sqLrLbc/imagwen.png`,
+      respuesta: `	La invención de la escritura.`,
+      distractores: [`	La aparición del hombre.`, `	La autonomía.`, `Dependencia política`],
+      ayuda: `También pintó la última cena`,
+      ayudaImg: `https://i.ibb.co/wgZZvKc/obras-de-arte-leonardo-da-vinci-google-498711013-153997319-1024x576.jpg`,
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/k91TZN3/img1.png` ,
+      pregunta: `Observa la imagen, lee el texto y responde la pregunta.`,
+      respuesta: `	Homo Sapiens Sapiens.`,
+      distractores: [`	Homo Erectus.`, `Homo Neanderthalensis.`, `	Homo Habilis.`],
+      ayuda: `es lo mismo que 2*2`,
+      ayudaImg: `https://i.ibb.co/k1WgGW2/descarga.jpg`,
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/2dhRjV3/img2.png` ,
+      pregunta: `Analiza las fuentes y luego responde`,
+      respuesta: `Fuente escrita y fuente visual.`,
+      distractores: [`Fuente audiovisual y fuente primaria.`, `Fuentes secundaria y fuente primaria`, `	Fuentes monumentales y fuentes escritas`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/w79TFcT/img3.png` ,
+      pregunta: `	Lee el siguiente texto, luego responde la pregunta.`,
+      respuesta: `	Cultura Chavín`,
+      distractores: [`	Imperio Wari`, `	Imperio Inca`, `		Cultura Moche`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/D5GnLqh/img4.png` ,
+      pregunta: `¿Cuál fue el proceso histórico que se desarrolló en primer lugar?`,
+      respuesta: `	Horizonte temprano.`,
+      distractores: [`	Horizonte medio.`, `	Intermedio temprano.`, `		Horizonte tardío.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/tZHyr2Z/img5.png` ,
+      pregunta: `Lee el siguiente texto, luego responde la pregunta.`,
+      respuesta: `	Killke.`,
+      distractores: [`	Incas.`, `	Nazca.`, `		Tiahuanaco.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/C2L67hv/img6.png` ,
+      pregunta: `	Lee el siguiente texto, luego responde la pregunta.`,
+      respuesta: `	Piedras, huesos y palos`,
+      distractores: [`	Teléfonos y computadoras`, `	Martillos y clavos`, `		Lápices y papel`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/3R78Ccn/img7.png` ,
+      pregunta: `Analiza el siguiente gráfico con atención y responde la pregunta.`,
+      respuesta: `	Culturas regionales y Formativo (influencia de la cultura Chavín)`,
+      distractores: [`Imperio Inca.`, `Arcaico y Lítico`, `	Imperio del Tahuantinsuyo, estados regionales e Imperio Huari`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/k16xywD/Whats-App-Image-2024-10-16-at-10-41-50-AM.jpg` ,
+      pregunta: `Lee el siguiente texto, luego responde la preginta`,
+      respuesta: `Forma de culto a los Muertos`,
+      distractores: [`	Chamanismo`, `	Animismo`, `	Costumbres, Creencias`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/1rL7PW5/Whats-App-Image-2024-10-16-at-10-55-34-AM.jpg` ,
+      pregunta: `Lee el siguiente texto, luego responde la pregunta`,
+      respuesta: `Todas las alternatvas`,
+      distractores: [`	Aseguraban victorias en sus batallas`, `Permitio grandes cambios en la sociedad.`, `	Permitio fundir y forjar metales para fabricar objetos para la agricultura`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/PxBbZSr/img11.jpg` ,
+      pregunta: `¿En qué imagen predomina los elementos naturales y en dónde los elementos sociales?`,
+      respuesta: `	Elementos naturales en el desierto y elementos sociales en las estribaciones andinas.`,
+      distractores: [`	Elementos naturales en el desierto y elementos sociales en la depresión.`, `	Elementos naturales en las estribaciones andinas y elementos sociales en las lomas.`, `		Elementos naturales en la depresión y elementos sociales en las lomas.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/1LdyMYH/img12.png` ,
+      pregunta: `¿Qué recursos naturales posee la costa, el mar peruano y que actividades económicas se realizan?`,
+      respuesta: `	Recursos naturales como algodón, peces, gas natural y se realizan las actividades económicas de la agricultura y la pesca.`,
+      distractores: [`		Recursos naturales como el caucho y se realiza la actividad económica de la extracción del petróleo.`, `	Actividades económicas como ganadería porcina, extracción de arroz y recursos naturales como pesca y navegación..`, `		Recursos naturales como el petróleo y se realizan la actividad económica de la extracción del caucho.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/84Tgsd3/img13.png` ,
+      pregunta: `	Lee el texto y responde`,
+      respuesta: `	El pueblo de Condoroma se fusiono con la mina y el agua contaminada no permitió la existencia de peces.`,
+      distractores: [`	Por la contaminación y la falta de agua en el pueblo.`, `	Por la presencia de la mina y el pueblo se mudó, porque en esa zona no había agua.`, `	Por la presencia de desechos mineros y el agua de Alto Condoroma está totalmente contaminada.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/44sYQQZ/img14.png` ,
+      pregunta: `	Observa la pirámide poblacional y responde`,
+      respuesta: `		En varones de 10 a 14 años y mujeres de 30 a 34 años.`,
+      distractores: [`		En varones y mujeres de 0 a 4 años.`, `		En mujeres de 50 a 54 años y varones de 70 a 74 años`, `		En mujeres de 0 a 4 años y varones de 20 a 24 años`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/7p9QQGT/img15.png` ,
+      pregunta: `		Observa el mapa y responde la pregunta.`,
+      respuesta: `		Latitud 50° y longitud 0°.`,
+      distractores: [`		Longitud 0° y latitud 40°..`, `		Se encuentra en el trópico de cáncer.`, `		Latitud 0° y hemisferio norte.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/JBWghFL/img16.png` ,
+      pregunta: `	Observa el mapa de zonas susceptibles de heladas en el Perú y responde`,
+      respuesta: `	Junín, Puno y Apurímac.`,
+      distractores: [`Cusco, puno y La Libertad.`, `	Lambayeque, Ucayali y Cusco.`, `Amazonas, Piura y Huancavelica.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/HPcGtHd/img17.png` ,
+      pregunta: `Lee y responde`,
+      respuesta: `		Falta de oxígeno, las erosiones, la temperatura del planeta se desequilibraría.`,
+      distractores: [`	El lavado de activos y la falsedad documental.`, `	La pérdida de biodiversidad y el crimen organizado.`, `	La abundancia de oxígeno, la absorción de dióxido de carbono y la seguridad alimentaria..`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/xFSrtPF/img18.jpg` ,
+      pregunta: `	De las necesidades presentadas en la imagen, cuáles serían las dos más importantes para la subsistencia del ser humano en cualquier medio:`,
+      respuesta: `		Alimentación y salud.`,
+      distractores: [`	Comunicación y ropa.`, `		Seguridad y comunicación.`, `	Ocio y alimentación.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/qdrQs9F/img19.jpg` ,
+      pregunta: `Marca la alternativa correcta, según la clasificación de bienes y servicios.`,
+      respuesta: `		La gasolina y el coche son bienes complementarios.`,
+      distractores: [`	El móvil es un bien público..`, `		La harina es un servicio.`, `La lavadora es un tipo de bien libre.`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/Df1ZJM1/img20.png` ,
+      pregunta: `De acuerdo al texto leído ¿Cuál es el rol del Estado en el desarrollo de la ciencia y la tecnología en el país?`,
+      respuesta: `	Fomentar la investigación.`,
+      distractores: [`	La prevención de desastres.`, `	Otorgar becas para estudiantes`, `	Aumentar PBI al sector educación.`],
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ];
